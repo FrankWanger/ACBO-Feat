fe2f2702cbb94230825daf04ea5b429d7f4c5fdb
Fanjin <fanjin.wang.20@ucl.ac.uk>
2024-03-26 10:40:52 +0000

    update functions

--- data_helper.py
+++ data_helper.py
@@ -1,19 +1,18 @@
-import deepchem as dc
 import numpy as np
-from deepchem.feat import CircularFingerprint, RDKitDescriptors, Mol2VecFingerprint, MordredDescriptors
 import os
 
 '''
 Summary:
     data_helper.py is a helper script that generates or load
     feature matrices and labels for the Lipophilicity dataset. 
-    It requires the DeepChem, mol2vec, mordred library to be 
-    installed. To avoid incompatibility issues, it is highly
-    recommended to use pre-featurized data. 
 
-    *Mordred fingerprint takes ~10 minutes to generate feature
+    gen_data_feat funciton requires the DeepChem, mol2vec, mordred 
+    library to be installed. To avoid incompatibility issues, it is 
+    highly recommended to use pre-featurized data. Mordred 
+    fingerprint takes ~10 minutes to generate featurize
     other fingerprints take ~1 minute to generate
 
+    load_lipo_feat function loads the pre-featurized data.
 
 Author(s):
     Fanjin Wang
@@ -24,7 +23,8 @@ Created:
 
 def gen_data_feat(featurizer_name='rdkit', save_data=False, save_filename='data/lipo_rdkit.csv',dataset = 'lipo'):
     '''
-    Generate the feature matrix and labels for the dataset.
+    Generate the feature matrix and labels for the dataset. Requires DeepChem/mol2vec/mordred to be installed. 
+    See readme file for more details.
 
     dataset: str
         Name of the dataset to use. Currently limited to 'lipo'.
@@ -38,6 +38,9 @@ def gen_data_feat(featurizer_name='rdkit', save_data=False, save_filename='data/
         y: np.ndarray
             Labels of shape (N,).
     '''
+    import deepchem as dc
+    from deepchem.feat import CircularFingerprint, RDKitDescriptors, Mol2VecFingerprint, MordredDescriptors
+
     # Instantiate featurizers
     if featurizer_name not in ['rdkit', 'ecfp', 'mol2vec', 'mordred']:
         raise ValueError('Invalid featurizer name. Options are rdkit, ecfp, mol2vec, mordred.')
@@ -49,6 +52,10 @@ def gen_data_feat(featurizer_name='rdkit', save_data=False, save_filename='data/
             'mordred': MordredDescriptors()
         }[featurizer_name]
 
+    # Check if the file already exists
+    if os.path.isfile(save_filename):
+        raise FileExistsError('File already exists. Please load from pre-featurized data or provide a different filename.')
+    
     # Load the Lipophilicity dataset
     if dataset == 'lipo':
         tasks, datasets, transformers = dc.molnet.load_lipo(featurizer)
@@ -59,10 +66,8 @@ def gen_data_feat(featurizer_name='rdkit', save_data=False, save_filename='data/
     # Get the feature matrix and labels 
     X, y = np.vstack((train_dataset.X,valid_dataset.X,test_dataset.X)),np.vstack((train_dataset.y,valid_dataset.y,test_dataset.y))
     if save_data:
-        if os.path.isfile(save_filename):
-            raise FileExistsError('File already exists. Please load from pre-featurized data or provide a different filename.')
-        else:
-            np.savetxt(save_filename, np.hstack((X,y)), delimiter=',')
+        np.savetxt(save_filename, np.hstack((X,y)), delimiter=',')
+
     return  X,np.ravel(y)
 
 def load_lipo_feat(filename='./data/lipo_rdkit.csv'):
@@ -76,23 +81,21 @@ def load_lipo_feat(filename='./data/lipo_rdkit.csv'):
             Labels of shape (N,).
     '''
     # Load the data
-
     if os.path.isfile(filename):
         data = np.loadtxt(filename, delimiter=',')
         X, y = data[:, :-1], data[:, -1] # Last column is the label
     else:
         raise FileNotFoundError('Pre-featurized data not found. Please use gen_data_feat instead.')
-
     return X, y
 
 if __name__ == '__main__':
 
     # Example usage
-    featurizer_name='mordred'
+    featurizer_name='rdkit'
     save_filename = os.path.join('data', 'lipo_{}.csv'.format(featurizer_name))
 
     # Generate and save the feature matrix and labels
-    #X, y = gen_data_feat(featurizer_name=featurizer_name, save_data=True, save_filename=save_filename)
+    # X, y = gen_data_feat(featurizer_name=featurizer_name, save_data=False, save_filename=save_filename)
 
     # or, load the pre-featurized data
     X, y = load_lipo_feat(filename=save_filename)
